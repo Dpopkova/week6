2adad7736623d90611553caad9e1f23874ac2414
Dpopkova <daria.ol.p@gmail.com>
2020-12-13 21:40:15 +0300

    Week 7

--- app.js
+++ app.js
@@ -1,43 +1,44 @@
-export default (express, bodyParser, createReadStream, crypto, http) => {
-    const app = express();  
-    app.use((req, res, next) => {
-    res.append('Access-Control-Allow-Origin', ['*']);
-    res.append('Access-Control-Allow-Methods', 'GET,POST,PUT,PATCH,OPTIONS,DELETE');
-    next();
+export default (express, bodyParser, createReadStream, crypto, http, connect) => {
+  const app = express();
+  app.use((req, res, next) => {
+  res.append('Access-Control-Allow-Origin', ['*']);
+  res.append('Access-Control-Allow-Methods', 'GET,POST,PUT,PATCH,OPTIONS,DELETE');
+  next();
 });
 
 app.get('/sha1/:input/', (req, res, next) => {
-    const shasum = crypto.createHash('sha1')
-    shasum.update(req.params.input)
-    res.send(shasum.digest('hex'))
+  const shasum = crypto.createHash('sha1')
+  shasum.update(req.params.input)
+  res.send(shasum.digest('hex'))
 })
 
 app.get('/login/', (req, res) => {
-  res.send('itmo283528')
+res.send('itmo283528')
 })
 
 app.get('/code/', (req, res) => {
-  const  reader = createReadStream(import.meta.url.substring(7))
-  reader.on('data', function (chunk) { 
-    res.send(chunk.toString()); 
-  }); 
+const  reader = createReadStream(import.meta.url.substring(7))
+reader.on('data', function (chunk) { 
+  res.send(chunk.toString()); 
+}); 
 
-  reader.on('error', function (chunk) { 
-        res.send('error')
-  }); 
+reader.on('error', function (chunk) { 
+      res.send('error')
+}); 
 })
 
 app.use(bodyParser.urlencoded({  
-  extended: true
+extended: true
 })); 
 
 app.post('/req/', (req, res) => { 
-     http.get( req.body.addr, (resFrom) => {
+   http.get( req.body.addr, (resFrom) => {
   const { statusCode } = resFrom;
   let error;
 
   if (statusCode !== 200) {
-    error = new Error('Request Failed.\n' + `Status Code: ${statusCode}`);
+    error = new Error('Request Failed.\n' +
+                      `Status Code: ${statusCode}`);
     resFrom.resume();
     return;
   } 
@@ -50,44 +51,54 @@ app.post('/req/', (req, res) => {
       res.send(rawData)
     } catch (e) {
       res.status(500)
-       res.send('error')
+      res.send('error')
     }
   });
+
     }).on('error', (e) => {
     res.status(500)
-     res.send('error')
+    res.send('error')
     }).end();
 
 })
 
-app.get('/req/', (req, res) => {    
-    http.get( req.query.addr, (resFrom) => {
-  const { statusCode } = resFrom;
-  let error;
+app.post('/insert/', async (req, res) => {
+const { login, password, URL} = req.body
+const conn = await connect(URL, {newUrlParser: true, useUnifiedTopology:true})
+const db = conn.db()
+const result = await db.collection('users').insertOne({login, password})
+res.send(`${login} ${password} ${URL}`)
+})
 
-  if (statusCode !== 200) {
-    error = new Error('Request Failed.\n' + `Status Code: ${statusCode}`);
-    resFrom.resume();
-    return;
-  } 
+app.get('/req/', (req, res) => {  
+http.get( req.query.addr, (resFrom) => {
+const { statusCode } = resFrom;
+let error;
 
-  resFrom.setEncoding('utf8');
-  let rawData = '';
-  resFrom.on('data', (chunk) => { rawData += chunk; });
-  resFrom.on('end', () => {
-    try {
-      res.send(rawData)
-    } catch (e) {
-      res.status(500)
-    }
-  });
-    }).on('error', (e) => {
+if (statusCode !== 200) {
+  error = new Error('Request Failed.\n' +
+                    `Status Code: ${statusCode}`);
+  resFrom.resume();
+  return;
+} 
+
+resFrom.setEncoding('utf8');
+let rawData = '';
+resFrom.on('data', (chunk) => { rawData += chunk; });
+resFrom.on('end', () => {
+  try {
+    res.send(rawData)
+  } catch (e) {
     res.status(500)
-    }).end();
+  }
+});
+  }).on('error', (e) => {
+  res.status(500)
+  }).end();
 })
 
 app.all('*', function( req, res) {
-    res.send('itmo283528')
+  res.send('itmo283528')
 })
 
 return app;
