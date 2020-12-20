6b13515d570f04fba2fb727977b8f2542d587aa9
Dpopkova <daria.ol.p@gmail.com>
2020-12-20 19:27:38 +0300

    week8

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 import bodyParser from 'body-parser'
 import http from 'http'
 import crypto from 'crypto'
-import { createReadStream } from 'fs'
+import { createReadStream, writeFileSync } from 'fs'
 import express from 'express';
 import mongo from 'mongodb';
 import appSrc from './app.js';
 const { MongoClient: { connect } } = mongo
-const app = appSrc(express, bodyParser, createReadStream, crypto, http, connect);
+const app = appSrc(express, bodyParser, createReadStream, crypto, http, connect, writeFileSync);
 app.listen(process.env.PORT);
